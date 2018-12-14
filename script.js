4968162ac5da52804ece55c0274e6b5fc0d49d2d
marcingal <marcin921125@gmail.com>
2018-12-14 12:05:17 +0100

    first 4 exercises made

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const arr1 = [2, 3, 5, 9, 20, 21]
 const arr2 = [2, 3, 5, 9, 20]
 const arr3 = [2, 3, 5, 9, 20, 35, 65, 19, 95, 87, 32]
 const arr4 = [1, -1, 38, 49, 2, 3, 5, 9, 20]
+const arr5 = []
 
 
 const arrayArithmeticAverage = array => array.reduce((reduced, element) => element = reduced + element
@@ -10,8 +11,31 @@ const arrayArithmeticAverage = array => array.reduce((reduced, element) => eleme
 
 const arrayMedian = array => {
     return (
-        !(array.length % 2)
-            ? (array[array.length / 2] + array[array.length / 2 + 1]) / 2
-            : array[Math.floor(array.length / 2)]
+        array.length === 0
+            ? 0
+            : array.sort((a, b) => a - b) &&
+            (array.length % 2
+                ? array[Math.floor(array.length / 2)]
+                : (array[array.length / 2] + array[array.length / 2 + 1]) / 2
+            )
     )
 }
+
+const arraySmallestNumber = array => {
+    return (
+        array.length === 0
+            ? 0
+            : array.sort((a, b) => a - b)[0]
+    )
+}
+
+const arrayBiggestNumber = array => {
+    return (
+        array.length === 0
+            ? 0
+            : array.sort((a, b) => a - b)[array.length - 1]
+    )
+}
+
+
+
